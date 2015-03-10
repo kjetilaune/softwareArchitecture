d3ac42dedebb84920ceccdd263188f8a0394ff52
Annie Aasen <aasenannie@gmail.com>
2015-03-10 14:35:03 +0100

    add GameSettings as a Singleton pattern

---
+++ Game/core/src/com/mygdx/game/model/GameSettings.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.model;
+
+/**
+ * Created by annieaa on 10/03/15.
+ */
+public class GameSettings {
+
+    private static GameSettings instance;
+    public int maxPlayers;
+    public ArrayList<Player> players;
+
+    private GameSettings() {
+
+    }
+
+    public static GameSettings getInstance() {
+        if (instance == null) {
+            instance = new GameSettings();
+        }
+        return instance;
+    }
+
+}
