10c1e9018ee986b6f628eaf1f2d10dc741cd7b5a
Kjetil Aune <aune.kjetil@gmail.com>
2015-03-11 13:26:39 +0100

    Annie, plz fix

--- Game/core/src/com/mygdx/game/gui/GameView.java
+++ Game/core/src/com/mygdx/game/gui/GameView.java
@@ -6,23 +6,51 @@ package com.mygdx.game.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
+import com.badlogic.gdx.graphics.g2d.PolygonSprite;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mygdx.game.MyGdxGame;
-import com.mygdx.game.model.Game;
+
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GameView implements Screen{
     private Stage stage;
+    SpriteBatch batch;
     MyGdxGame game;
+    ShapeRenderer shapeRenderer = new ShapeRenderer();
+
+
+    int height = Gdx.graphics.getHeight();
 
     public GameView(MyGdxGame game){
         stage = new Stage();
         this.game = game;
+        batch = new SpriteBatch();
 
     }
 
+
     public void generateBackground(){
-        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClearColor((float)(198.0/255.0), (float)(226.0/255.0), 1, 1);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.rect(50, 50, 50, 50);
+        for (int i = 0; i < Gdx.graphics.getWidth() - 1; i++){
+
+            shapeRenderer.setColor((float)(160.0/255.0), (float)(219.0/255.0), (float)(142.0/255.0), 1);
+            shapeRenderer.rect(i, 50, 50, 50);
+
+        }
+        shapeRenderer.end();
+
     }
 
     @Override
@@ -32,8 +60,12 @@ public class GameView implements Screen{
 
     @Override
     public void render(float delta) {
+        batch.begin();
         generateBackground();
+        batch.end();
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         stage.act();
         stage.draw();
     }
