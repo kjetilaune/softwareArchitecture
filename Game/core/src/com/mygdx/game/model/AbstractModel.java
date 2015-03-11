74b1f3292fa7b7421994f2720265a1faa2b852b8
Jonathan Trapnes <jbtrapne@stud.ntnu.no>
2015-03-11 11:53:19 +0100

    add AbstractModel

---
+++ Game/core/src/com/mygdx/game/model/AbstractModel.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.model;
+
+/**
+ * Created by Jonathan on 11.03.2015.
+ */
+
+import com.badlogic.gdx.utils.Json; //don't know if we should use this or the java.io
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public abstract class AbstractModel implements Json.Serializable{
+
+    protected PropertyChangeSupport propChangeSupport;
+
+    public AbstractModel(){
+        propChangeSupport = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+        propChangeSupport.addPropertyChangeListener(listener);
+    }
+    public void removePropertyChangeListener(PropertyChangeListener listener){
+        propChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue){
+        propChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+
+
+}
