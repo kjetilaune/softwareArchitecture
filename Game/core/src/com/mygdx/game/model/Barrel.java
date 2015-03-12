d38e1d4acd13fdddadc2d6ae24974d88bf154a6b
Nikola Radenkovic <nixa92nixa@gmail.com>
2015-03-12 16:01:35 +0100

    Barrel and Texture Manager added

---
+++ Game/core/src/com/mygdx/game/model/Barrel.java
@@ -0,0 +1,29 @@
+package com.mygdx.game.model;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Nixa on 3/12/2015.
+ */
+
+public class Barrel extends GameObject {
+    private float angle;
+    TextureRegion tr;
+    public Barrel (Vector2 rootPos, float angle){
+        super();
+        this.position = rootPos;
+        this.angle = angle;
+        tr = new TextureRegion(TextureManager.barrel);
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(tr, position.x, position.y, position.x, position.y, (float) TextureManager.barrel.getWidth(), (float) TextureManager.barrel.getHeight(), 1, 1, angle);
+    }
+
+public void setAngle(float angle){
+    this.angle = angle;
+    }
+}
---
+++ Game/core/src/com/mygdx/game/model/TextureManager.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Nixa on 3/12/2015.
+ */
+public class TextureManager {
+    public static Texture barrel = new Texture(Gdx.files.internal("Object/barrel.png"));
+}
