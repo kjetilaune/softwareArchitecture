748e83672882bddf36ae8662c119a56e1e228cc1
Jonathan Trapnes <jbtrapne@stud.ntnu.no>
2015-03-12 14:02:59 +0100

    Think we need this for MVC (?)

--- Game/core/src/com/mygdx/game/controller/AbstractController.java
+++ Game/core/src/com/mygdx/game/controller/AbstractController.java
@@ -1,8 +1,43 @@
 package com.mygdx.game.controller;
 
+import com.mygdx.game.model.AbstractModel;
+import java.beans.PropertyChangeListener;
+
+import sun.plugin.dom.views.AbstractView;
+
 /**
  * Created by Jonathan on 10.03.2015.
  */
-public class AbstractController {
-    //helloWorld
+public abstract class AbstractController implements PropertyChangeListener{
+
+    private AbstractView registeredView;
+    private AbstractModel registeredModel;
+
+    public AbstractController(){
+
+    }
+
+    public void addModel(AbstractModel model){
+        registeredModel = model;
+        model.addPropertyChangeListener(this);
+    }
+
+    public void removeModel(AbstractModel model){
+        registeredModel = null;
+        model.removePropertyChangeListener(this);
+    }
+
+    public void addView(AbstractView view){
+        registeredView = view;
+    }
+    public void removeView(AbstractView view){
+        registeredView = null;
+    }
+    public AbstractModel getRegisteredModel(){
+        return registeredModel;
+    }
+    public AbstractView getRegisteredView(){
+        return registeredView;
+    }
+    //methods for propertyChangeEvents and setModelPropertiy
 }
