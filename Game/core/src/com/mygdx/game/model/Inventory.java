52ba875b2d03efcfe01da76f220ec4ffee452494
mikaelbj <mikaelbj@stud.ntnu.no>
2015-03-12 13:38:01 +0100

    Updated game logic (Store + Inventory)

--- Game/core/src/com/mygdx/game/model/Inventory.java
+++ Game/core/src/com/mygdx/game/model/Inventory.java
@@ -7,60 +7,105 @@ import java.util.HashMap;
  */
 public class Inventory {
 
-    private HashMap<Ammunition, Integer> ammunitions;
-    private HashMap<String, Integer> upgrades;
+    // Maps name of the Ammunition to the inventory amount
+    private HashMap<String, Integer> ammunitions;
 
-    public HashMap<Ammunition, Integer> getAmmunitions() {
-        return ammunitions;
+    // Maps name of the Ammunition to the inventory amount
+    private HashMap<String, Integer> upgrades;
+    private int score;
+
+    public Inventory() {
+        score = 0;
+
+        // set default upgrades
+        upgrades.put("Armor", 0);
+        upgrades.put("Health", 0);
+        upgrades.put("Fuel", 0);
+
+        // set default ammunition
+        ammunitions.put("Bullet", 50);
+        ammunitions.put("F-Bomb", 5);
+        ammunitions.put("Instakill", 1);
+
     }
 
-    public void setAmmunitions(HashMap<Ammunition, Integer> ammunitions) {
-        this.ammunitions = ammunitions;
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public HashMap<String, Integer> getAmmunitions() {
+        return ammunitions;
     }
 
     public HashMap<String, Integer> getUpgrades() {
         return upgrades;
     }
 
-    public void setUpgrades(HashMap<String, Integer> upgrades) {
-        this.upgrades = upgrades;
+    public void increaseAmmo(String ammo, int amount) {
+        if (!ammunitions.containsKey(ammo)) {
+            ammunitions.put(ammo, amount);
+        }
+        else {
+            int currentAmount = ammunitions.get(ammo);
+            ammunitions.put(ammo, currentAmount + amount);
+        }
     }
 
-    public boolean increaseAmmo(Ammunition ammo, int amount) {
-        int n = ammunitions.get(ammo);
-        if (n <= 0) {
+    public boolean decreaseAmmo(String ammo, int amount) {
+        if (!ammunitions.containsKey(ammo)) {
             return false;
         }
-        ammunitions.put(ammo, n + amount);
+
+        int currentAmount = ammunitions.get(ammo);
+        if (currentAmount - amount <= 0) {
+            return false;
+        }
+        ammunitions.put(ammo, currentAmount - amount);
         return true;
     }
 
-    public boolean decreaseAmmo(Ammunition ammo, int amount) {
-        int n = ammunitions.get(ammo);
-        if (n - amount <= 0) {
-            return false;
-        }
-        ammunitions.put(ammo, n - amount);
-        return true;
-    }
-
-    public boolean increaseUpgrade(String upgrade, int amount) {
-        int n = upgrades.get(upgrade);
-        if (n <= 0) {
-            return false;
-        }
-        upgrades.put(upgrade, n + amount);
-        return true;
+    public void increaseUpgrade(String upgrade, int amount) {
+        int currentAmount = upgrades.get(upgrade);
+        upgrades.put(upgrade, currentAmount + amount);
     }
 
     public boolean decreaseUpgrade(String upgrade, int amount) {
-        int n = upgrades.get(upgrade);
-        if (n - amount <= 0) {
+        int currentAmount = upgrades.get(upgrade);
+        if (currentAmount - amount <= 0) {
             return false;
         }
-        upgrades.put(upgrade, n - amount);
+        upgrades.put(upgrade, currentAmount - amount);
         return true;
     }
 
+    public boolean buyAmmunition(String ammo, Integer amount) {
+        int cost = Store.getAmmunitionPrice(ammo) * amount;
+
+        if (cost > score) {
+            System.out.println("Player does not have the score to purchase this item!");
+            return false;
+        }
+
+        score -= cost;
+        increaseAmmo(ammo, amount);
+        return true;
+    }
+
+    public boolean buyUpgrade(String upgrade, Integer amount) {
+        int cost = Store.getUpgradePrice(upgrade) * amount;
+
+        if (cost > score) {
+            System.out.println("Player does not have the score to purchase this item!");
+            return false;
+        }
+
+        score -= cost;
+        increaseUpgrade(upgrade, amount);
+        return true;
+    }
 
 }
