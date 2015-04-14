0b6d5c5c864e00f493955ac34d4c785ad856a758
Annie Aasen <aasenannie@gmail.com>
2015-04-14 18:36:32 +0200

    restructure assets

--- Game/core/src/com/mygdx/game/model/TextureManager.java
+++ Game/core/src/com/mygdx/game/model/TextureManager.java
@@ -7,30 +7,30 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by Nixa on 3/12/2015.
  */
 public class TextureManager {
-    public static Texture barrel = new Texture(Gdx.files.internal("object/barrel.png"));
-    public static Texture tank = new Texture(Gdx.files.internal("object/tank.png"));
+    public static Texture barrel = new Texture(Gdx.files.internal("objects/old/barrel.png"));
+    public static Texture tank = new Texture(Gdx.files.internal("objects/old/tank.png"));
 
-    public static Texture barrelSweets = new Texture(Gdx.files.internal("design/Barrel_Sweets.png"));
-    public static Texture tankSweets = new Texture(Gdx.files.internal("design/Tank_Sweets.png"));
-    public static Texture barrelFastfood = new Texture(Gdx.files.internal("design/Barrel_Fastfood.png"));
-    public static Texture tankFastfood = new Texture(Gdx.files.internal("design/Tank_Fastfood.png"));
-    public static Texture barrelVegan = new Texture(Gdx.files.internal("design/Barrel_Vegan.png"));
-    public static Texture tankVegan = new Texture(Gdx.files.internal("design/Tank_Vegan.png"));
-    public static Texture barrelSeafood = new Texture(Gdx.files.internal("design/Barrel_Seafood.png"));
-    public static Texture tankSeafood = new Texture(Gdx.files.internal("design/Tank_Seafood.png"));
+    public static Texture barrelSweets = new Texture(Gdx.files.internal("objects/vehicles/Barrel_Sweets.png"));
+    public static Texture tankSweets = new Texture(Gdx.files.internal("objects/vehicles/Tank_Sweets.png"));
+    public static Texture barrelFastfood = new Texture(Gdx.files.internal("objects/vehicles/Barrel_Fastfood.png"));
+    public static Texture tankFastfood = new Texture(Gdx.files.internal("objects/vehicles/Tank_Fastfood.png"));
+    public static Texture barrelVegan = new Texture(Gdx.files.internal("objects/vehicles/Barrel_Vegan.png"));
+    public static Texture tankVegan = new Texture(Gdx.files.internal("objects/vehicles/Tank_Vegan.png"));
+    public static Texture barrelSeafood = new Texture(Gdx.files.internal("objects/vehicles/Barrel_Seafood.png"));
+    public static Texture tankSeafood = new Texture(Gdx.files.internal("objects/vehicles/Tank_Seafood.png"));
 
-    public static Texture burgerBullet = new Texture(Gdx.files.internal("object/burgerBullet.png"));
-    public static Texture muffinBullet = new Texture(Gdx.files.internal("object/muffinBullet.png"));
+    public static Texture burgerBullet = new Texture(Gdx.files.internal("objects/old/burgerBullet.png"));
+    public static Texture muffinBullet = new Texture(Gdx.files.internal("objects/old/muffinBullet.png"));
 
     public static Texture grass = new Texture(Gdx.files.internal("textures/grass.png"));
     public static Texture sand = new Texture(Gdx.files.internal("textures/sand.png"));
     public static Texture ice = new Texture(Gdx.files.internal("textures/ice.png"));
     public static Texture rock = new Texture(Gdx.files.internal("textures/rock.png"));
 
-    public static Texture titleBackground = new Texture(Gdx.files.internal("design/Food-Frenzy-Logo.png"));
-    public static Texture menuBackground = new Texture(Gdx.files.internal("design/Food-Frenzy-Menu.png"));
-    public static Texture settingsBackground = new Texture(Gdx.files.internal("design/Food-Frenzy-Settings.png"));
-    public static Texture aboutBackground = new Texture(Gdx.files.internal("design/Food-Frenzy-About.png"));
-    public static Texture storeBackground = new Texture(Gdx.files.internal("design/Food-Frenzy-Store.png"));
+    public static Texture titleBackground = new Texture(Gdx.files.internal("backgrounds/Food-Frenzy-Logo.png"));
+    public static Texture menuBackground = new Texture(Gdx.files.internal("backgrounds/Food-Frenzy-Menu.png"));
+    public static Texture settingsBackground = new Texture(Gdx.files.internal("backgrounds/Food-Frenzy-Settings.png"));
+    public static Texture aboutBackground = new Texture(Gdx.files.internal("backgrounds/Food-Frenzy-About.png"));
+    public static Texture storeBackground = new Texture(Gdx.files.internal("backgrounds/Food-Frenzy-Store.png"));
 
 }
