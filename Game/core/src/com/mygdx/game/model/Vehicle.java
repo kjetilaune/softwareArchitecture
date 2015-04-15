f5a3a5a4b9970594e11a8abc8373c9591a1f6fe9
Annie Aasen <aasenannie@gmail.com>
2015-04-15 14:29:43 +0200

    add method for checking collision with a vehicle

--- Game/core/src/com/mygdx/game/model/Vehicle.java
+++ Game/core/src/com/mygdx/game/model/Vehicle.java
@@ -2,7 +2,11 @@ package com.mygdx.game.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.Vector;
 
 /**
  * Created by annieaa on 10/03/15.
@@ -55,7 +59,63 @@ public abstract class Vehicle extends GameObject {
     }
 
     public boolean isColliding(Vector2 point) {
-        // check if ammo hits tank
-        return true;
+
+        Polygon boundingBox;
+
+        float[] vertices = new float[8];
+
+        vertices[0] = getPosition().x;
+        vertices[1] = getPosition().y;
+        vertices[2] = rotatePoint(getPosition(), new Vector2(getPosition().x, getPosition().y + getTexture().getHeight()), getRotation()).x;
+        vertices[3] = rotatePoint(getPosition(), new Vector2(getPosition().x, getPosition().y + getTexture().getHeight()), getRotation()).y;
+        vertices[4] = rotatePoint(getPosition(), new Vector2(getPosition().x + getTexture().getWidth(), getPosition().y + getTexture().getHeight()), getRotation()).x;
+        vertices[5] = rotatePoint(getPosition(), new Vector2(getPosition().x + getTexture().getWidth(), getPosition().y + getTexture().getHeight()), getRotation()).y;
+        vertices[6] = rotatePoint(getPosition(), new Vector2(getPosition().x + getTexture().getWidth(), getPosition().y), getRotation()).x;
+        vertices[7] = rotatePoint(getPosition(), new Vector2(getPosition().x + getTexture().getWidth(), getPosition().y), getRotation()).y;
+
+        boundingBox = new Polygon(vertices);
+
+        // check if ammo hits tank.
+        return boundingBox.contains(point.x, point.y);
+    }
+
+    private float getRelativeHeight() {
+
+        float leftX = getPosition().x;
+        float leftY = getPosition().y;
+        float heightX = leftX;
+        float heightY = leftY + getTexture().getHeight();
+
+        return rotatePoint(new Vector2(leftX, leftY), new Vector2(heightX, heightY), getRotation()).y - leftY;
+    }
+
+
+    // rotate a point 'p' around the point 'pivot' by an angle of 'rotation'
+    public Vector2 rotatePoint(Vector2 pivot, Vector2 p, float rotation) {
+        float pivotX = pivot.x;
+        float pivotY = pivot.y;
+        float pX = p.x;
+        float pY = p.y;
+
+        float s = (float)Math.sin(rotation * Math.PI / 180);
+        float c = (float)Math.cos(rotation * Math.PI / 180);
+
+        // translate point back to origin
+        pX -= pivotX;
+        pY -= pivotY;
+
+        // rotate point
+        float newPX = pX * c - pY * s;
+        float newPY = pX * s + pY * c;
+
+        // translate point back
+        pX = newPX + pivotX;
+        pY = newPY + pivotY;
+
+        return new Vector2(pX, pY);
+    }
+
+    private float getRelativeWidth() {
+        return 0;
     }
 }
