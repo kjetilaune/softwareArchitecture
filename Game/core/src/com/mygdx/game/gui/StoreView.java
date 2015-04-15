cfc2d00568a737fb4624d7f5b33c1c462b69040f
Kjetil Aune <aune.kjetil@gmail.com>
2015-04-15 12:06:44 +0200

    Add image in store and button functionality

--- Game/core/src/com/mygdx/game/gui/StoreView.java
+++ Game/core/src/com/mygdx/game/gui/StoreView.java
@@ -3,6 +3,8 @@ package com.mygdx.game.gui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -13,8 +15,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.model.Ammunition;
 import com.mygdx.game.model.Game;
 import com.mygdx.game.model.Player;
+import com.mygdx.game.model.TextureManager;
 
 import java.util.ArrayList;
 
@@ -26,6 +30,8 @@ public class StoreView implements Screen{
     private Game gameInstance;
     private Player currentPlayer;
     private ArrayList<Player> players;
+    private ArrayList<Ammunition> ammos;
+    private Ammunition currentAmmo;
     private int numberOfPlayers;
 
     //GUI
@@ -43,12 +49,17 @@ public class StoreView implements Screen{
     private TextButton buy;
     private ImageButton arrowLeft, arrowRight;
 
+    private Sprite sprite, currentAmmoSprite;
+    private SpriteBatch batch;
+
     public StoreView(MyGdxGame game, Game gameInstance){
         this.game = game;
         this.gameInstance = gameInstance;
         this.players = gameInstance.getPlayers();
         this.currentPlayer = players.get(0);
         numberOfPlayers = players.size();
+        ammos = currentPlayer.getInventory().getAmmunitions();
+        currentAmmo = currentPlayer.getChosenAmmo();
 
         //GUI
         stage = new Stage();
@@ -97,6 +108,7 @@ public class StoreView implements Screen{
         txtCurrentPlayer = new Label("Player 1", skin);
         txtCurrentPlayer.setFontScale(2);
         txtMoney = new Label("$" + currentPlayer.getMoney(), skin);
+        currentAmmoSprite = getCurrentSprite();
 
         /*
         container.add(placeholderLabel).prefHeight(stage.getHeight()/10 * 2).prefWidth(stage.getWidth());
@@ -115,17 +127,24 @@ public class StoreView implements Screen{
         container.add(txtMoney).top().padTop(stage.getHeight()/10).padRight(stage.getWidth()/10);
         container.row().fillX();
 
-        ammoContainer.add(arrowLeft).left().maxWidth(stage.getWidth()/20).padLeft(stage.getWidth()/10);
-        ammoContainer.add(placeholderLabel).prefWidth(stage.getWidth()).prefHeight(stage.getHeight()/10 * 5);
-        ammoContainer.add(arrowRight).right().maxWidth(stage.getWidth()/20).padRight(stage.getWidth()/10);
+        ammoContainer.add(arrowLeft).left().maxWidth(stage.getWidth() / 20).padLeft(stage.getWidth() / 10);
+        ammoContainer.add(placeholderLabel).prefWidth(stage.getWidth()).prefHeight(stage.getHeight() / 10 * 5);
+        ammoContainer.add(arrowRight).right().maxWidth(stage.getWidth() / 20).padRight(stage.getWidth() / 10);
 
         bottomContainer.row();
-        bottomContainer.add(currentPlayerLabel).prefWidth(stage.getWidth()/20 * 7).prefHeight(stage.getHeight()/10 * 1).bottom().padTop(stage.getHeight()/10 * 7);
+        bottomContainer.add(currentPlayerLabel).prefWidth(stage.getWidth()/20 * 7).prefHeight(stage.getHeight()/10 * 1).bottom().padTop(stage.getHeight() / 10 * 7);
         bottomContainer.row();
         bottomContainer.add(txtCurrentPlayer).prefWidth(stage.getWidth()/20 * 7).prefHeight(stage.getHeight()/10 * 3).padLeft(stage.getWidth()/20).bottom();
-        bottomContainer.add(buy).prefWidth(stage.getWidth()/20 * 4);
-        bottomContainer.add(back).prefWidth(stage.getWidth()/20 * 8).padLeft(stage.getWidth()/20).padRight(stage.getWidth()/20);
+        bottomContainer.add(buy).prefWidth(stage.getWidth() / 20 * 4);
+        bottomContainer.add(back).prefWidth(stage.getWidth()/20 * 8).padLeft(stage.getWidth() / 20).padRight(stage.getWidth() / 20);
 
+        sprite = new Sprite(TextureManager.storeBackground);
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        currentAmmoSprite.setPosition(stage.getWidth()/20 * 6, stage.getHeight()/10 * 5 - currentAmmoSprite.getHeight()/2);
+        currentAmmoSprite.setScale(10);
+
+        batch = new SpriteBatch();
 
     }
 
@@ -136,6 +155,7 @@ public class StoreView implements Screen{
                 //game.setScreen(new MainMenu(game, 100));
                 if (currentPlayer == players.get(players.size()-2)){
                     currentPlayer = players.get(players.size()-1);
+                    txtMoney.setText("" + currentPlayer.getMoney());
                     txtCurrentPlayer.setText("Player " + (players.indexOf(currentPlayer) + 1));
                     back.setText("New round");
                 }
@@ -144,18 +164,61 @@ public class StoreView implements Screen{
                 }
                 else{
                     currentPlayer = players.get(players.indexOf(currentPlayer));
+                    txtMoney.setText("" + currentPlayer.getMoney());
                     txtCurrentPlayer.setText("Player " + (players.indexOf(currentPlayer) + 1));
                 }
+            }
+        });
+
+        arrowLeft.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (ammos.indexOf(currentAmmo) == 0){
+                    currentAmmo = ammos.get(ammos.size() - 1);
+
+                }
+                else{
+                    currentAmmo = ammos.get(ammos.indexOf(currentAmmo) - 1);
+                }
+
+
+            }
+        });
+
+        arrowRight.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (ammos.indexOf(currentAmmo) == ammos.size()-1){
+                    currentAmmo = ammos.get(0);
+                }
+                else{
+                    currentAmmo = ammos.get(ammos.indexOf(currentAmmo) + 1);
+                }
+
             }
         });
 
         Gdx.input.setInputProcessor(stage);
     }
 
+    private Sprite getCurrentSprite(){
+        System.out.println("Sprite" + currentPlayer.getTeam().getAmmunitionSprite(currentPlayer.getChosenAmmo().getName()));
+        return currentPlayer.getTeam().getAmmunitionSprite(currentPlayer.getChosenAmmo().getName());
+    }
+
 
     public void render (float delta){
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        currentAmmoSprite = getCurrentSprite();
+        currentAmmoSprite.setPosition(stage.getWidth()/20 * 6, stage.getHeight()/10 * 5 - currentAmmoSprite.getHeight()/2);
+        currentAmmoSprite.setScale(10);
+
+        batch.begin();
+        sprite.draw(batch);
+        currentAmmoSprite.draw(batch);
+        batch.end();
 
         stage.act();
         stage.draw();
