e8e73f733cf44b15331b338e7fd35ac23169e169
mikaelbj <mikaelbj@stud.ntnu.no>
2015-04-21 12:38:36 +0200

    Sound was removed, added it again

--- Game/core/src/com/mygdx/game/model/AmmoTypes/DeliBomb.java
+++ Game/core/src/com/mygdx/game/model/AmmoTypes/DeliBomb.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.model.AmmoTypes;
 
 import com.mygdx.game.model.Ammunition;
+import com.mygdx.game.model.SoundManager;
 
 /**
  * Created by Mikal on 20.04.2015.
@@ -8,7 +9,7 @@ import com.mygdx.game.model.Ammunition;
 public class DeliBomb extends Ammunition {
 
     public DeliBomb() {
-        super("DeliBomb", 15, 150, 1.1f);
+        super("DeliBomb", 15, 150, 1.1f, SoundManager.impact2);
     }
 
 }
--- Game/core/src/com/mygdx/game/model/AmmoTypes/Instastuffed.java
+++ Game/core/src/com/mygdx/game/model/AmmoTypes/Instastuffed.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.model.AmmoTypes;
 
 import com.mygdx.game.model.Ammunition;
+import com.mygdx.game.model.SoundManager;
 
 /**
  * Created by Mikal on 12.03.2015.
@@ -8,7 +9,7 @@ import com.mygdx.game.model.Ammunition;
 public class Instastuffed extends Ammunition {
 
     public Instastuffed() {
-        super("Instastuffed", 100, 20, 1.4f);
+        super("Instastuffed", 100, 20, 1.4f, SoundManager.instaYummy);
     }
 
 }
